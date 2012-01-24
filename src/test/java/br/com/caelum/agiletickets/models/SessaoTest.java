5ef5b8b8a2dd5b87a8cd9422bcc62c0a899708f2
Ticiane <ticiane.ottoni@cgu.gov.br>
2012-01-24 11:40:46 -0200

    Refatoracao da classe SessaoTest.java incluindo o metodo Inicializar

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,30 +1,32 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	private Sessao sessao;
+	
+	@Before
+	public void inicializar(){
+		sessao = new Sessao();
+	}
 
 	@Test
-	public void deveVender1ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
+	public void deveVender1ingressoSeHa2vagas() throws Exception {		
         sessao.setTotalIngressos(2);
-
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
 	public void naoDeveVender3ingressoSeHa2vagas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
-
 		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
 
 		sessao.reserva(3);
@@ -32,8 +34,7 @@ public class SessaoTest {
 	}
 	
 	@Test
-	public void reservarTodosOsIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
+	public void deveReservarTodosOsIngressosDisponiveis() throws Exception {
 		sessao.setTotalIngressos(5);
 		Assert.assertEquals(true, sessao.podeReservar(5));
 	}
